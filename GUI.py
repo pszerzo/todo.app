c03be629cfd0cdcda8ff678c7179bafc6d738cac
Peter <peterszerzo@gmail.com>
2025-01-01 18:43:43 +0100

    implemented complete and exit

--- GUI.py
+++ GUI.py
@@ -9,8 +9,10 @@ add_button = frs.Button("Add")
 list_box = frs.Listbox(values=f.get_todos(), key="todos",
                        enable_events=True, size=[45, 10])
 edit_button = frs.Button("Edit")
+complete_button = frs.Button("Complete")
+exit_button = frs.Button("Exit")
 
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
+layout = [[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
 window = frs.Window("My To-Do App", layout=layout,
                     font=("Helvetica", 22))
 
@@ -36,6 +38,18 @@ while True:
             print(todos)
             f.write_todos(todos)
             window["todos"].update(values=todos)
+        case "Complete":
+            todo_completed = values["todos"][0]
+            todos = f.get_todos()
+            # index = todos.index(todo_complete)
+            # todos.pop(index)
+            todos.remove(todo_completed)
+            print(todos)
+            f.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case "todos":
             window["todo"].update(value=values["todos"][0])
         case frs.WIN_CLOSED:
--- bonus/another.py
+++ bonus/another.py
@@ -194,15 +194,24 @@
 # print(number)
 
 
-#converter GUI
-import FreeSimpleGUI as fr
+import FreeSimpleGUI as sg
 
-label1 = fr.Text("Enter feet")
-label2 = fr.Text("Enter inches")
-input1 = fr.Input()
-input2 = fr.Input()
-convert1 = fr.Button("Convert")
+# Prepare the widgets for the left column
+left_column_content = [[sg.Text('Left 1')],
+                       [sg.Text('Left 2')]]
 
-window = fr.Window("Convertor", layout=[[label1, input1], [label2, input2],[convert1]])
+# Prepare the widgets for the right column
+right_column_content = [[sg.Text('Right 1')],
+                        [sg.Text('Right 2')]]
+
+# Construct the Column widgets
+left_column = sg.Column(left_column_content)
+right_column = sg.Column(right_column_content)
+
+# Construct the layout
+layout = [[left_column, right_column]]
+
+# Construct and display the window
+window = sg.Window('Columns', layout)
 window.read()
 window.close()
